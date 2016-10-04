18ab362ad7b9ba587de259f241e7539401621f26
David <Jimbo>
2016-10-04 15:15:54 -0400

    Add project files.

---
+++ ReadFile/JRDLib.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ReadFile
+{
+    /// <summary>
+    /// STANDARD FLAT FILE PROPERTY CLASS
+    /// </summary>
+    public class JRDFile
+    {
+        string fileName { get; set; }
+        string folderPath { get; set; }
+        string filePath { get; set; }
+        string fileType { get; set; }
+        int fixedWidth { get; set; }
+        bool isFixedWidth { get; set; }
+        int numFields { get; set; }
+        string delimeter { get; set; }
+        bool hasDelimeter { get; set; }
+        string findText { get; set; }
+        bool hasFindText { get; set; }
+        int findTextIndex { get; set; }
+        string[] fields;
+        //JRDFileField fields;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <param name="folderPath"></param>
+    public JRDFile(string fileName, string folderPath)
+    {
+        this.fileName = fileName;
+        this.folderPath = folderPath;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <param name="folderPath"></param>
+    /// <param name="fileType"></param>
+    /// <param name="numFields"></param>
+    /// <param name="findText"></param>
+    /// <param name="findeTextIndex"></param>
+    public JRDFile(string fileName, string folderPath,string fileType, int numFields,string findText,int findeTextIndex)
+    {
+        this.fileName = fileName;
+        this.folderPath = folderPath;
+        this.filePath = folderPath + fileName + fileType;
+        this.fileType = fileType;
+        this.numFields = numFields;
+        this.findText = findText;
+        this.findTextIndex = findTextIndex;
+        fields = new string [numFields];
+    }
+
+
+    public void readFile(JRDFile file)
+    {
+        using (StreamReader reader = new StreamReader(file.filePath))
+        {
+            string currLine = "";
+            string[] fields = new string[file.numFields]; 
+            while ((currLine = reader.ReadLine()) != null)
+            {
+                    if(Regex.Split(currLine,@"\s{2,}").Length == numFields)
+                {
+                    fields = Regex.Split(currLine, @"\s{2,}");
+
+                    if (fields[findTextIndex].Equals(findText))
+                    {
+                        writeToFile(file, currLine);
+                    }//if
+                }//if
+            }//while
+        }//using
+    }//method
+
+    public void writeToFile(JRDFile file, string outputString)
+    {   
+        using (StreamWriter writer = new StreamWriter(file.filePath + ".txt", true))
+        {
+            writer.WriteLine(outputString);
+
+        }//using
+    }//method
+
+}//CLASS
+
+
+
+
+    class JRDLibSqlConnection
+    {
+    }
+
+    
+}
---
+++ ReadFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadFile
+{
+  
+    class Application
+    { 
+        static void Main(string[] args)
+        {
+            string folderPath = @"C:\Users\djimbo\Documents\";
+            string fileName = "JETRO.NACHA.Nacha";
+            string fileType = ".2016003";
+            string findText = "627021000021031009719";
+            int numFields = 3;
+            int findTextIndex = 0;
+
+            JRDFile read = new JRDFile(fileName,folderPath,fileType,numFields,findText,findTextIndex);
+
+
+            read.readFile(read);
+
+            //read.writeToFile(read,"Testing 123");
+        }
+    }
+}
