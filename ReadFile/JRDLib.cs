424639da2b0c573a40aca6a7d592c589c6fa06e1
Roscoe <david.jimbo@JetroRD.com>
2016-10-13 12:32:12 -0400

    Added helper classes, changed parser structure

--- ReadFile/JRDLib.cs
+++ ReadFile/JRDLib.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Net.Mail;
 
 namespace ReadFile
 {
@@ -17,83 +18,124 @@ namespace ReadFile
         string folderPath { get; set; }
         string filePath { get; set; }
         string fileType { get; set; }
+        string[] outputFields { get; set; }
+        string outputFileName { get; set; }
+        string outputFolderPath { get; set; }
+        string outputPath { get; set; }
+        List <string> outputLines { get; set; }
         JRDTextFinder [] textFinder;
-        int fieldsToFind;
-        //int fixedWidth { get; set; }
-        //bool isFixedWidth { get; set; }
-        //int numFields { get; set; }
-        //string findText { get; set; }
-        //bool hasFindText { get; set; }
-        //int findTextIndex { get; set; }
-        //JRDFileField fields;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <param name="folderPath"></param>
-    public JRDFile(string fileName, string folderPath)
-    {
-        this.fileName = fileName;
-        this.folderPath = folderPath;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <param name="folderPath"></param>
-    /// <param name="fileType"></param>
-    /// <param name="numFields"></param>
-    /// <param name="findText"></param>
-    /// <param name="findeTextIndex"></param>
-    public JRDFile(string fileName, string folderPath,string fileType,JRDTextFinder [] textFinder)
-    {
-        this.fileName = fileName;
-        this.folderPath = folderPath;
-        this.filePath = folderPath + fileName + fileType;
-        this.fileType = fileType;
-        //this.fieldsToFind = fieldsToFind;
-        this.textFinder = textFinder;
-    }
-
-
-    public void readFile(JRDFile file)
-    {
-        using (StreamReader reader = new StreamReader(file.filePath))
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="folderPath"></param>
+        public JRDFile(string fileName, string folderPath)
         {
+            this.fileName = fileName;
+            this.folderPath = folderPath;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="folderPath"></param>
+        /// <param name="fileType"></param>
+        /// <param name="numFields"></param>
+        /// <param name="findText"></param>
+        /// <param name="findeTextIndex"></param>
+        public JRDFile(string fileName, string folderPath,string fileType,JRDTextFinder [] textFinder)
+        {
+            this.fileName = fileName;
+            this.folderPath = folderPath;
+            this.filePath = folderPath + fileName + fileType;
+            this.fileType = fileType;
+            this.textFinder = textFinder;
+            outputFields = new string[textFinder.Length + 1];
+            outputLines = new List<string>();
+            outputFolderPath = folderPath + @"Processed\";
+            
+
+        }
+
+        public JRDFile(string filePath,JRDTextFinder [] textFinder)
+        {
+                this.filePath = filePath;
+                this.textFinder = textFinder;
+                fileName = Path.GetFileName(filePath);
+                outputFields = new string[textFinder.Length + 1];
+                outputLines = new List<string> ();
+                outputFolderPath = folderPath + @"Processed\";
+        }
+
+        //@todo OPTIMIZE THIS CODE
+        public List<string> readFiles(JRDFile file)
+        {
+
             string currLine = "";
             int numConstraints = file.textFinder.Length;
-             
-            //WE MAY HAVE NUMEROUS CONSTRAINTS PER FILE
-            for(int i = 0; i < numConstraints; i++)
+
+            foreach (string directoryPath in Directory.GetFiles(file.folderPath, file.fileName + file.fileType))
             {
-                string[] fields = new string[file.textFinder[i].fieldCount];
-                while ((currLine = reader.ReadLine()) != null)
+                using (StreamReader reader = new StreamReader(directoryPath))
                 {
-                    if (Regex.Split(currLine, file.textFinder[i].delimeter).Length == file.textFinder[i].fieldCount)
+                    while ((currLine = reader.ReadLine()) != null)
                     {
-                        fields = Regex.Split(currLine, file.textFinder[i].delimeter);
-                        if (fields[file.textFinder[i].fieldIndex].Equals(file.textFinder[i].findText))
+                        //WE MAY HAVE NUMEROUS CONSTRAINTS PER FILE
+                        for (int i = 0; i < numConstraints; i++)
                         {
-                            writeToFile(file, currLine);
-                        }//if
-                    }//if
-                }//while
-            }
-        }//using
-    }//method
+                            string[] fields = new string[file.textFinder[i].fieldCount];
 
-    public void writeToFile(JRDFile file, string outputString)
-    {   
-        using (StreamWriter writer = new StreamWriter(file.filePath + ".txt", true))
+                            if (Regex.Split(currLine, file.textFinder[i].delimeter).Length == file.textFinder[i].fieldCount)
+                            {
+                                fields = Regex.Split(currLine, file.textFinder[i].delimeter);
+                                for (int j = 0; j < fields.Length; j++)
+                                {
+                                    if (fields[j].Equals(file.textFinder[i].findText))
+                                    {
+                                        file.outputFields[i] = fields[file.textFinder[i].fieldIndex];
+                                    }//if
+                                }//for
+                            }//if
+                        }//for
+                    }//while                
+                }//using
+                outputFields[outputFields.Length - 1] = Path.GetFileName(directoryPath);
+                outputLines.Add(createOutputline(file.outputFields));
+                //writeToFile(file,Path.GetFileName(directoryPath), file.outputFields);
+            }//for
+            return outputLines;
+        }//method
+
+        private string createOutputline(string [] outputStrings)
         {
-            writer.WriteLine(outputString);
+            StringBuilder builder = new StringBuilder();
+            foreach (string outputString in outputStrings )
+            {
+                outputString.Replace(",", "");
+                builder.Append(outputString + ", ");
+            }
+            return builder.ToString().Trim().Substring(0, builder.ToString().Trim().Length - 1);
+        }
 
-        }//using
-    }//method
+        public void writeToFile(bool includeFileName,List <string> outputLines)
+        {
+            StringBuilder builder = new StringBuilder();
+            Directory.CreateDirectory(this.outputFolderPath);
+            using (StreamWriter writer = new StreamWriter( outputFolderPath + DateTime.Now.ToString("yyyy_MM_dd_mm_") +"ParsedFile.txt"))
+            {
+                foreach (string outputLine in outputLines)
+                {
+                    if (includeFileName)
+                        writer.WriteLine(outputLine);
+                    else
+                        writer.WriteLine(outputLine.Substring(outputLine.IndexOf(","), outputLine.Length - outputLine.IndexOf(",")));
+                }
+            }
+        }
 
-}//CLASS
+    }//class
 
    public class JRDTextFinder
     {
@@ -109,7 +151,7 @@ namespace ReadFile
         public int fieldIndex;
         public string findText;
         public string delimeter;
-        string[] fields;
+        public string fieldToFind;
 
 
         public JRDTextFinder()
@@ -128,6 +170,7 @@ namespace ReadFile
             this.isFixedWidth = false;
             this.hasFindText = false;
             this.hasDelimeter = false;
+            this.fieldToFind = null;
         }
 
         public JRDTextFinder(int fieldCount, int fieldIndex,string findText,string delimeter)
@@ -142,14 +185,97 @@ namespace ReadFile
             this.isLineNumber = false;
             this.hasFindText = true;
             this.hasDelimeter = true;
-            fields = new string[fieldCount];
-        }
+            this.fieldToFind = null;
+        }  
     }
 
-
-    class JRDLibSqlConnection
+    public class JRDHelpers
     {
-    }
 
-    
+        public void sendEmail(string subject, string body, string sender, string[] recipients)
+        {
+
+            SmtpClient smtp = new SmtpClient("outlook.corporate.jetrord.com", 25);
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(sender);
+            mail.IsBodyHtml = true;
+            mail.Subject = subject;
+            mail.Body = body;
+
+            foreach (string recepient in recipients)
+            {
+                mail.To.Add(recepient);
+            }
+
+            smtp.Send(mail);
+        }
+
+        //@overload default sender here
+        public void sendEmail(string subject, string body, string[] recipients)
+        {
+
+            SmtpClient smtp = new SmtpClient("outlook.corporate.jetrord.com", 25);
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("support@JetroRD.com");
+            mail.IsBodyHtml = true;
+            mail.Subject = subject;
+            mail.Body = body;
+
+            foreach (string recepient in recipients)
+            {
+                mail.To.Add(recepient);
+            }
+
+            smtp.Send(mail);
+        }
+
+        public string convertMainFrameDate(string format, string date)
+        {
+            string convertedDate = "";
+            switch (format.ToLower())
+            {
+                case "yyyymmdd":
+                    convertedDate = date.Trim().Substring(4, 2) + "/" + date.Trim().Substring(date.Length - 2, 2)
+                   + "/" + date.Trim().Substring(0, 4);
+                    break;
+                case "yymmdd":
+                    convertedDate = date.Trim().Substring(2, 2) + "/" + date.Trim().Substring(date.Length - 2, 2)
+                    + "/" + date.Trim().Substring(0,2);
+                    break;
+                default:
+                    convertedDate = "";
+                    break;
+            }
+
+            try
+            {
+                 Convert.ToDateTime(convertedDate);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Bad date format will deafault to" + Convert.ToDateTime("1/1/1900"));
+                return "1/1/1900";
+            }
+
+            return Convert.ToDateTime(convertedDate).ToShortDateString();
+        }
+
+        public string getSubstring(bool afterStr,string text ,string findText)
+        {
+            try
+            {
+                if (afterStr)
+                {
+                    return text.Substring(text.IndexOf(findText), text.Length - text.IndexOf(findText));
+                }
+                else return text.Substring(0, text.IndexOf(findText));
+            }catch(Exception e)
+            {
+                Console.WriteLine("Search Pattern '" + findText + "'not found in text '" + text + "'");
+                return text;
+            }
+        }
+
+    }
 }
--- ReadFile/Program.cs
+++ ReadFile/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ReadFile
 {
@@ -11,39 +12,41 @@ namespace ReadFile
     { 
         static void Main(string[] args)
         {
-            //FILE 
-            string folderPath = @"C:\Users\djimbo\Documents\";
-            string fileName = "JETRO.NACHA.Nacha";
-            string fileType = ".2016003";
+          
+            //MULTI FILE INFO
+            string folderPath1 = @"C:\Users\djimbo.CORPORATE\Desktop\ParsingApp\To Chase\To Chase\";
+            string fileName1 = "JETRO.NACHA.Nacha";
+            string fileType1 = "*.2016*";
 
             //FILE CONSTRAINTS 0
             int fieldCount0 = 3;
             int fieldIndex0 = 1;
             string findText0 = "627021000021031009719";
-            string findDelimeter0 = @"\s{1,}";
+            string findDelimeter0 = @"\s{2,}";
 
             //FILE CONSTRAINTS 1
             int fieldCount1 = 4;
-            int fieldIndex1 = 3;
+            int fieldIndex1 = 2;
             string findText1 = "5200JETRO C & C ENT.";
-            string findDelimeter1 = @"\s{1,}";
+            string findDelimeter1 = @"\s{2,}";
 
+            //INITIALIZE JRD FILE CONSTRAINTS
             JRDTextFinder finder0 = new JRDTextFinder(fieldCount0, fieldIndex0, findText0, findDelimeter0);
             JRDTextFinder finder1 = new JRDTextFinder(fieldCount1, fieldIndex1, findText1, findDelimeter1);
 
-            //LIST FOR JRD FILE CONSTRUCTOR
-
+            //ARRAY FOR JRD FILE CONSTRUCTOR
             JRDTextFinder[] textFinders = new JRDTextFinder[2];
-
             textFinders[0] = finder0;
             textFinders[1] = finder1;
 
 
-            JRDFile read = new JRDFile(fileName,folderPath,fileType,));
+            JRDFile file = new JRDFile(fileName1, folderPath1, fileType1, textFinders);
+            //file.readFiles(file);
+            List<string> test = file.readFiles(file);
+            file.writeToFile(true,test);
 
-
-            read.readFile(read);
-
+            JRDHelpers helper = new JRDHelpers();
+            
         }
     }
 }
